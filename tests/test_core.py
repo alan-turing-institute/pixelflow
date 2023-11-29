7fef303c02d93aa27b857e13c87d5bf205864ac4
Isabel Fenton <ifenton@turing.ac.uk>
2023-11-29 09:36:53 +0000

    Linting

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,7 +1,9 @@
-import pytest
-import pixelflow
+"""Tests for core functionality of pixelflow."""
 
+import pytest
 from skimage.measure import label
+
+import pixelflow
 from ._utils import simulate_image
 
 
@@ -21,13 +23,12 @@ def test_core_count(simulated_dataset):
     assert isinstance(result, pixelflow.core.PixelflowResult)
     assert result.count() == num_blobs
 
+
 @pytest.mark.parametrize("dim", (2, 3))
 def test_core_dim_labels(dim):
     """Test dimension guessing flags incorrect number"""
     size = (128,) * dim
     img, coords = simulate_image(size=size, num_blobs=5)
     mask = label(img > 2.3)
-    with pytest.raises(ValueError ,match=r"dim_labels doesn't match mask dimensions"):
-        pixelflow.pixelflow(mask, features = ('bbox',), dim_labels = 'X' * (dim + 1))
-    
-    
+    with pytest.raises(ValueError, match=r"dim_labels doesn't match mask dimensions"):
+        pixelflow.pixelflow(mask, features=("bbox",), dim_labels="X" * (dim + 1))
