86a197b0438db99f0a306bc37402f1cc3bf2cb38
Isabel Fenton <ifenton@turing.ac.uk>
2024-01-11 11:35:33 +0000

    Making the image file larger and the cutoff slightly lower so that the blobs are more likely to be separate objects

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,10 +8,10 @@ from ._utils import simulate_image
 @pytest.fixture(params=[2, 3])
 def simulated_dataset(request):
     """Make a simulated dataset."""
-    size = (128,) * request.param
+    size = (256,) * request.param
     num_blobs = 5
     img, coords = simulate_image(size=size, num_blobs=num_blobs)
-    mask = label(img > 2.3)
+    mask = label(img > 2.2)
     props = regionprops(mask)
     bbox = [prop.bbox for prop in props]
     return mask, img, coords, bbox
